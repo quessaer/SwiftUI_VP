eb4102b9833d6f4dfb232e543a3a9b28f9854481
quessaer <quessaer@gmail.com>
2021-11-22 11:48:52 +0900

    popover 추가

---
+++ VestPIN_SwiftUI Project/Views/Categories/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  VestPIN_SwiftUI Project
+//
+//  Created by J.T. Kim on 2021/11/22.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
