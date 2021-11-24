2b48f6481b276ca33af5cedab6ea19877f00494f
quessaer <quessaer@gmail.com>
2021-11-24 22:09:35 +0900

    2021_11_24 Vikie 연동중...

---
+++ VestPIN_SwiftUI Project/Views/LoginViews/PinLoginView2.swift
@@ -0,0 +1,20 @@
+//
+//  PinLoginView2.swift
+//  VestPIN_SwiftUI Project
+//
+//  Created by J.T. Kim on 2021/11/24.
+//
+
+import SwiftUI
+
+struct PinLoginView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PinLoginView2_Previews: PreviewProvider {
+    static var previews: some View {
+        PinLoginView2()
+    }
+}
