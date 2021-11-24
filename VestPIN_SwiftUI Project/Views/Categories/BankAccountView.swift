fad544e0ed85215ae70fe9a01f48b000e9fc0085
quessaer <quessaer@gmail.com>
2021-11-24 22:22:02 +0900

    commit

--- VestPIN_SwiftUI Project/Views/Categories/BankAccountView.swift
+++ VestPIN_SwiftUI Project/Views/Categories/BankAccountView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct BankAccountView: View {
+    enum Tab {
+        case categoryHome
+        case register
+        case auth
+        case sign
+        case renew
+        case remove
+    }
+
+    
+    
+    @State private var selection: Tab = .register
+    // 등록된 사용자의 숫자가 탭뷰에 뱃지로 표시되도록 변수 추가
+    var registeredUserNumber = 3
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selection) {
+            AccountPageView()
+                .badge(registeredUserNumber)
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("메인")
+                }
+                .tag(0)
+            
+            AccountHistoryView()
+                .tabItem {
+                    Label ("이체내역", systemImage: "person.badge.plus")
+                }
+                .tag(1)
+        
+            ChangeView()
+                .tabItem {
+                    Label ("전자서명", systemImage: "square.and.pencil")
+                }
+                .tag(2)
+            
+//            RemoveView()
+//                .tabItem {
+//                    Label ("삭제", systemImage: "trash.fill")
+//                }
+//                .tag(3)
+
+            OptionView()
+                .tabItem {
+                    Label ("설정 페이지", systemImage: "gearshape.fill")
+                }
+                .tag(4)
+        }
     }
 }
 
